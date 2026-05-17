fa3dbe0f85abe2980e6877e2d56623439f87f72f
anandukb <kbanandu@applab.qa>
2026-05-17 15:49:21 +0530

    error fix

--- src/components/TawkMessenger.tsx
+++ src/components/TawkMessenger.tsx
@@ -8,7 +8,7 @@ export default function TawkMessenger() {
       id="tawk-messenger"
       strategy="lazyOnload"
       src="https://embed.tawk.to/6a0991720a71ec1c34c47cf3/1joqm0a92"
-      crossOrigin="*"
+      crossOrigin="anonymous"
     />
   );
 }
